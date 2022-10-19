bb30098d509bd569abb735d7c2869b14ab87afd8
drKreso <kresimir.bojcic@gmail.com>
2022-10-19 09:32:19 +0200

    libcrypto

--- ext/fastpbkdf2_native/extconf.rb
+++ ext/fastpbkdf2_native/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 
 $CFLAGS += " -Wno-deprecated-declarations -std=c99 -O3 -g -Wall -pedantic"
-$LOCAL_LIBS += " -libcrypto"
+$LOCAL_LIBS += " -libcrypto.a"
 
 extension_name = "fastpbkdf2_native"
 
